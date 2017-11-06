a83d07def212550698459805d55a03dc47c36712
Brett Harvey <brettharvey2014@gmail.com>
2017-11-06 15:16:25 -0400

    Update lab7.cpp

--- Labs/lab7.cpp
+++ Labs/lab7.cpp
@@ -1,3 +1,10 @@
+//============================================================================
+// Name        : Lab7.cpp
+// Author      : Brett Harvey
+// Version     : 1.0
+// Description :   Write a client program to execute a single HTTP GET on any type of content. The requested file will be stored locally.
+//============================================================================
+
 #include <winsock2.h>
 #include <WS2tcpip.h>
 #include <windows.h>
@@ -5,7 +12,8 @@
 #pragma comment(lib,"ws2_32.lib")
 using namespace std;
 int main(){
-	// Initialize Dependencies to the Windows Socket.
+
+	/* Initialize Dependencies to the WinSocket. */
 	WSADATA wsaData;
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
 		cout << "WSAStartup failed.\n";
@@ -13,17 +21,17 @@ int main(){
 		return -1;
 	}
 
-	// We first prepare some "hints" for the "getaddrinfo" function
-	// to tell it, that we are looking for a IPv4 TCP Connection.
+	/* We first prepare some "hints" for the "getaddrinfo" function
+	to tell it, that we are looking for a IPv4 TCP Connection. */
 	struct addrinfo hints;
 	ZeroMemory(&hints, sizeof(hints));
 	hints.ai_family = AF_INET;          // We are targeting IPv4
 	hints.ai_protocol = IPPROTO_TCP;    // We are targeting TCP
 	hints.ai_socktype = SOCK_STREAM;    // We are targeting TCP so its SOCK_STREAM
 
-	// Aquiring of the IPv4 address of a host using the newer
-	// "getaddrinfo" function which outdated "gethostbyname".
-	// It will search for IPv4 addresses using the TCP-Protocol.
+	/* Aquiring of the IPv4 address of a host using the newer
+	"getaddrinfo" function which outdated "gethostbyname".
+	 It will search for IPv4 addresses using the TCP-Protocol. */
 	struct addrinfo* targetAdressInfo = NULL;
 	DWORD getAddrRes = getaddrinfo("cse.stfx.ca", NULL, &hints, &targetAdressInfo);
 	if (getAddrRes != 0 || targetAdressInfo == NULL)
@@ -34,18 +42,17 @@ int main(){
 		return -1;
 	}
 
-	// Create the Socket Address Informations, using IPv4
-	// We dont have to take care of sin_zero, it is only used to extend the length of SOCKADDR_IN to the size of SOCKADDR
+	/* Create the Socket Address Informations, using IPv4
+	We dont have to take care of sin_zero, it is only used to extend the length of SOCKADDR_IN to the size of SOCKADDR */
 	SOCKADDR_IN sockAddr;
 	sockAddr.sin_addr = ((struct sockaddr_in*) targetAdressInfo->ai_addr)->sin_addr;    // The IPv4 Address from the Address Resolution Result
 	sockAddr.sin_family = AF_INET;  // IPv4
 	sockAddr.sin_port = htons(80);  // HTTP Port: 80
 
-	// We have to free the Address-Information from getaddrinfo again
+	/* We have to free the Address-Information from getaddrinfo again */
 	freeaddrinfo(targetAdressInfo);
 
-	// Creation of a socket for the communication with the Web Server,
-	// using IPv4 and the TCP-Protocol
+	/* Creation of a socket for the communication with the Web Server using IPv4 and the TCP-Protocol */
 	SOCKET webSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (webSocket == INVALID_SOCKET)
 	{
@@ -55,7 +62,7 @@ int main(){
 		return -1;
 	}
 
-	// Establishing a connection to the web Socket
+	/* Establishing a connection to the web Socket */
 	cout << "Connecting...\n";
 	if (connect(webSocket, (SOCKADDR*)&sockAddr, sizeof(sockAddr)) != 0)
 	{
@@ -67,7 +74,7 @@ int main(){
 	}
 	cout << "Connected.\n";
 
-	// Sending a HTTP-GET-Request to the Web Server
+	/* Sending a HTTP-GET-Request to the Web Server */
 	const char* httpRequest = "GET http://cse.stfx.ca/~pdsec13/index.php HTTP/1.1\r\nHost: cse.stfx.ca\r\nConnection: close\r\n\r\n";
 	int sentBytes = send(webSocket, httpRequest, strlen(httpRequest), 0);
 	if (sentBytes < strlen(httpRequest) || sentBytes == SOCKET_ERROR)
@@ -79,7 +86,7 @@ int main(){
 		return -1;
 	}
 
-	// Receiving and Displaying an answer from the Web Server
+	/* Receiving and Displaying an answer from the Web Server */
 	char buffer[10000];
 	ZeroMemory(buffer, sizeof(buffer));
 	int dataLen;
@@ -92,7 +99,7 @@ int main(){
 		}
 	}
 
-	// Cleaning up Windows Socket Dependencies
+	/* Cleaning up Windows Socket Dependencies */
 	closesocket(webSocket);
 	WSACleanup();
 
